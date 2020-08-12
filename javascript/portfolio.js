4c50967ad2b962f94b8586f9e6bf0c960dcc6438
James Wong <wongwongjames@yahoo.com.hk>
2020-08-13 02:53:14 +0800

    portfolio.html timeline enhance

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,28 +1,38 @@
 var timelineEvents;
+var timelineEventsArr = [];
 var description;
+var descriptionArr = [];
+var currentShowProjectId;
 
 window.addEventListener("load", function () {   
-    timelineEvents = document.querySelectorAll('.timeline ol li div');
-    description = document.querySelectorAll('.descriptionContainer div');
-    console.log(timelineEvents);
-    console.log(description);
-	
+    timelineEvents = document.querySelectorAll(".timeline ol li div");    
+    description = document.querySelectorAll(".descriptionContainer div");
+    
+    for(var i = 0;i<timelineEvents.length;i++){
+        timelineEvents[i].parentElement.style.width = Math.floor(100/timelineEvents.length) + "%";
+        timelineEventsArr.push(timelineEvents[i].id);
+        descriptionArr.push(description[i].id);
+    }
 });
 
 function showContent(element_id){
-    console.log(element_id);
+    var index = descriptionArr.indexOf(element_id);
+
+    if(currentShowProjectId == element_id && description[index].className == " descriptionActive"){
+        timelineEvents[index].parentElement.className = timelineEvents[index].className.replace(" active", "");
+        description[index].className = description[index].className.replace(" descriptionActive", "");
+        description[index].style.display = "none";
+        return;
+    }
+
     for(var i = 0;i<description.length;i++){
         timelineEvents[i].parentElement.className = timelineEvents[i].className.replace(" active", "");
         description[i].className = description[i].className.replace(" descriptionActive", "");
         description[i].style.display = "none";
     }
 
-    for(var i = 0;i<description.length;i++){
-        if(description[i].id == element_id){
-            timelineEvents[i].parentElement.className += " active";
-            description[i].className += " descriptionActive";
-            description[i].style.display = "block";
-            break;
-        }
-    }
+    timelineEvents[index].parentElement.className += " active";
+    description[index].className += " descriptionActive";
+    description[index].style.display = "block";
+    currentShowProjectId = element_id;
 }
