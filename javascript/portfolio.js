a1dd1bfee47b4b53abe7d9b1e6ce482d41777fdc
James Wong <wongwongjames@yahoo.com.hk>
2020-08-14 00:42:46 +0800

    code tidying, change portfolio picture

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,39 +1,46 @@
 var timelineEvents;
 var timelineEventsArr = [];
-var description;
-var descriptionArr = [];
+var descriptionModal;
+var descriptionModalArr = [];
 var currentShowProjectId;
 
-window.addEventListener("load", function () {   
+window.addEventListener("load", function (){   
     timelineEvents = document.querySelectorAll(".timeline ol li div");    
-    description = document.querySelectorAll(".modal");
+    descriptionModal = document.querySelectorAll(".modal");
     
     for(var i = 0;i<timelineEvents.length;i++){
         timelineEvents[i].parentElement.style.width = Math.floor(100/timelineEvents.length) + "%";
         timelineEventsArr.push(timelineEvents[i].id);
-        descriptionArr.push(description[i].id);
-    }
+        descriptionModalArr.push(descriptionModal[i].id);
 
+        /*add onclick function to the timeline text*/
+        timelineEvents[i].addEventListener("click", function(){
+            showContent(this.id);
+        });
+        /*add onclick function to the timeline text*/
+    }
 });
 
 function showContent(element_id){
-    var index = descriptionArr.indexOf(element_id);
-    description[index].style.display = "block";
+    var index = descriptionModalArr.indexOf(element_id);
+    descriptionModal[index].style.display = "block";
     currentShowProjectId = element_id;
     document.body.style.overflow = 'hidden';
 
+    /*add close function to the button*/
     document.querySelectorAll(".close")[index].addEventListener("click", function(){
-        var index = descriptionArr.indexOf(currentShowProjectId);
-        description[index].style.display = "none";
+        var index = descriptionModalArr.indexOf(currentShowProjectId);
+        descriptionModal[index].style.display = "none";
         document.body.style.overflow = 'auto';
     });
+    /*add close function to the button*/
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    var index = descriptionArr.indexOf(currentShowProjectId);
-    if (event.target == description[index]) {
-        description[index].style.display = "none";
+window.onclick = function(event){
+    var index = descriptionModalArr.indexOf(currentShowProjectId);
+    if (event.target == descriptionModal[index]) {
+        descriptionModal[index].style.display = "none";
         document.body.style.overflow = 'auto';
     }
 }
