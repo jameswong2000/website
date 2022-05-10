9d0f6df2d180d6b6a9b87ce528ecc5c8b9cca108
James Wong <wongwongjames@yahoo.com.hk>
2022-05-11 00:10:18 +0800

    header amend

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,23 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Header() {
     return (
-        <header>
-            <Navbar variant="dark" expand="sm">
-                <Container fluid>
-                    <Navbar.Brand>
-                        <img id="logo" alt="Logo" src={Logo} className="d-inline-block"/>
-                        <Navbar.Text>James Wong</Navbar.Text>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="nav-links" />
-                    <Navbar.Collapse id="nav-links"className="justify-content-end">
-                        <Nav>
-                            <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-                            <Nav.Link href="#Gallery">Gallery</Nav.Link>
-                            <Nav.Link href="#About Me">About Me</Nav.Link>
-                            <Nav.Link href="#About Webpage">About Webpage</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
+        <Navbar variant="dark" expand="sm">
+            <Container fluid>
+                <Navbar.Brand>
+                    <img id="logo" alt="Logo" src={Logo} className="d-inline-block"/>
+                    <Navbar.Text>James Wong</Navbar.Text>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="nav-links" />
+                <Navbar.Collapse id="nav-links"className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="#gallery">Gallery</Nav.Link>
+                        <Nav.Link href="#about me">About Me</Nav.Link>
+                        <Nav.Link href="#about webpage">About Webpage</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
